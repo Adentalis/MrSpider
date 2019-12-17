044411ae2ca399f811df7cae1000e9b7295483b8
Daniel <daniel.n96@t-online.de>
2019-12-17 23:30:55 +0100

    different training modes implemented

--- cube/cubex.js
+++ cube/cubex.js
@@ -1,14 +1,21 @@
 const GoogleSpreadsheet = require('google-spreadsheet');
 const { promisify } = require('util');
-const readline = require('readline');
+
 
 const creds = require('./client_secret.json');
 
 let words = [];
 
 //---------------------GAME PARAMETERS
-const AMOUNT_OF_WORDS = 10;
-const TIME_BETWEEN_EACH_WORD = 1000; //in ms
+const AMOUNT_OF_WORDS = 50;
+const TIME_BETWEEN_EACH_WORD = 4000; //in ms
+
+/*  ----GAMEMODES---
+    1 = 10 Wörter kommen nacheinander und einfach merken
+    2 = es kommt alle x Sekunden eine Abkürzung 
+
+*/
+const GAMEMODE = 2;
 
 async function accessSpreadsheet() {
     console.log("Get Data from G-Sheet");
@@ -24,6 +31,7 @@ async function accessSpreadsheet() {
         'max-row': 26,
 
         //column = das von links nach rechts
+
         'min-col': 3,
         'max-col': 24
     });
@@ -37,35 +45,53 @@ async function accessSpreadsheet() {
         });
     });
 
-    playGame();
+    checkWords();
+
+    switch (GAMEMODE) {
+        case 1:
+            startShow(0, getRandom(words, AMOUNT_OF_WORDS));
+            break;
+        case 2: show1ShortcutAfterAnother(0, getRandom(words, AMOUNT_OF_WORDS));
+            break;
+        default:
+            break;
+    }
 
 }
 
 accessSpreadsheet();
 
-function playGame() {
-    console.log("---Start Game---");
 
-    const choosenWords = getRandom(words, AMOUNT_OF_WORDS);
 
-    start(0, choosenWords);
-
-    var rl = readline.createInterface(process.stdin, process.stdout);
-    rl.question("", function (answer) {
-        console.log(answer);
-    })
-}
-
-function start(counter, words) {
+//---------------------GAMEMODE 1---------------------
+function startShow(counter, words) {
     if (counter < AMOUNT_OF_WORDS) {
         setTimeout(function () {
             counter++;
             console.log(counter, words[counter - 1].value);
-            start(counter, words);
-        }, 1000);
+            startShow(counter, words);
+        }, TIME_BETWEEN_EACH_WORD);
     }
 }
 
+//---------------------GAMEMODE 2---------------------
+function show1ShortcutAfterAnother(c, words) {
+
+    setTimeout(function () {
+        c++;
+        console.log(words[c].shortcut);
+        show1ShortcutAfterAnother(c, words);
+
+        setTimeout(function () {
+            console.log(words[c].value);
+            console.log("------------")
+        }, TIME_BETWEEN_EACH_WORD / 2);
+
+    }, TIME_BETWEEN_EACH_WORD);
+
+}
+
+//get n random elements of an array
 function getRandom(arr, n) {
     var result = new Array(n),
         len = arr.length,
@@ -78,5 +104,15 @@ function getRandom(arr, n) {
         taken[x] = --len in taken ? taken[len] : len;
     }
     return result;
+}
+
+//check everyword if its shortcut has 2 Uppercase letters
+function checkWords() {
+    console.log("chek all words ")
+    words.forEach(e => {
+        if (e.shortcut.length !== 2) {
+            console.log(e.value, "ist falsch!!")
+        }
+    })
 }
 
